2c19060ecdeb2a2adba3a01a194a93cc0f8bd9b0
condo <condo@meerchat.mobi>
2015-03-13 22:58:24 -0400

    Initialized main and create pages

--- app/src/main/java/com/eventchat/MainActivity.java
+++ app/src/main/java/com/eventchat/MainActivity.java
@@ -1,17 +1,44 @@
-package com.eventchat;
+package mobi.meerchat.eventchat;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ListView eventListView;
+    ArrayList<String> eventList;
+    ArrayAdapter<String> aa; //for events list
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eventListView = (ListView)findViewById(R.id.listItems);
+        eventList = new ArrayList<String>();
+        //in reality, get this stuff via web request
+        eventList.add("Jazz Concert\nBo Diddley - 8PM");
+        eventList.add("Florida Gators\nOConnel Center - 9PM");
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,eventList);
+        eventListView.setAdapter(aa);
+
+
+
     }
 
 
@@ -35,5 +62,19 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void showEventCategories(View v) {
+        Log.v("TEST", "put popup here");
+        //may use dialog, but may quit and use intents
+    }
+
+    public void makeEvent(View v) {
+        Log.v("TEST","go to create event");
+        Intent makeIntent = new Intent(this,CreateEvent.class);
+        startActivity(makeIntent);
+
     }
 }
