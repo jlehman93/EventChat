069a3fb88071ecc2e77ef78011998e3108222433
condo <condo@meerchat.mobi>
2015-03-14 05:00:13 -0400

    Intro to reading

--- app/src/main/java/com/eventchat/MainActivity.java
+++ app/src/main/java/com/eventchat/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,7 +19,10 @@ import android.widget.ListView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import org.json.JSONArray;
 
@@ -43,11 +47,35 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         eventListView = (ListView)findViewById(R.id.listItems);
         eventList = new ArrayList<String>();
         //in reality, get this stuff via web request
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("eventObject");
+        query.whereEqualTo("category", 0); //get things from a specific category
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> returnedEventList, ParseException e) {
+                if (e == null) {
+                    Log.d("score", "Retrieved " + returnedEventList.size() + " scores");
+                    //eventList=returnedEventList;
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+
         eventList.add("Jazz Concert\nBo Diddley - 8PM");
         eventList.add("Florida Gators\nOConnel Center - 9PM");
         aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,eventList);
         eventListView.setAdapter(aa);
         categoryList = new ArrayList<String>(Arrays.asList("General", "Live Music", "Sports"));
+
+        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Log.v("TEST","a"+position);
+                Intent eventIntent = new Intent(context,EventPage.class);
+                startActivity(eventIntent);
+
+            }
+        });
 
         /**
         JSONArray myArray = new JSONArray();
@@ -88,6 +116,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+
     public void onClick(View v) {
         //trying to handle clicking on list
         eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -95,11 +124,11 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                                     int position, long id) {
 
                 eventInt = position;
-                Toast.makeText(getApplicationContext(),
-                        eventList.get(position) + " selected", Toast.LENGTH_SHORT).show();
+                Log.v("TEST",""+eventInt);
             }
         });
     }
+
 
     public void showEventCategories(View v) {
         Log.v("TEST", "put popup here");
